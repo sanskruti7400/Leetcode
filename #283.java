70aefe278cba8ca205234dbc2e32e5a5831a1355
Sanskruti Dhanaji Kadam <sanskadam7400@gmail.com>
2025-03-20 00:12:23 +0530

    Update #283.java

--- #283.java
+++ #283.java
@@ -1,3 +1,4 @@
+//first method
 class Solution {
     public void moveZeroes(int[] nums) {
         int k=0;
@@ -8,6 +9,27 @@ class Solution {
         }
         while(k<nums.length){
             nums[k++]=0;
+        }
+    }
+}
+
+
+
+//second method 
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int[] temp=new int[nums.length];
+        int k=0;
+        for(int num : nums){
+            if(num!=0){
+                temp[k++]=num;
+            }
+        }
+        while(k<nums.length){
+            temp[k++]=0;
+        }
+        for(int i=0;i<nums.length;i++){
+            nums[i]=temp[i];
         }
     }
 }
