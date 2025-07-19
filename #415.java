b24b588652d67bea86a5d4fc7bd2f38f95867682
Sanskruti Dhanaji Kadam <sanskadam7400@gmail.com>
2025-07-19 10:16:43 -0700

    Create #415.java

---
+++ #415.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+class Solution {
+    public String addStrings(String num1, String num2) {
+        BigInteger n1=new BigInteger(num1);
+        BigInteger n2=new BigInteger(num2);
+        BigInteger sum=n1.add(n2);
+        String n3=sum.toString();
+        return n3;
+    }
+}
