bebf4d388818197f74d028f8e1ce79e55a5854e0
Sanskruti Dhanaji Kadam <sanskadam7400@gmail.com>
2026-01-20 22:57:40 +0530

    Create #503.java

---
+++ #503.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < 2 * n; i++) {
+            int curr = nums[i % n];
+
+            while (!stack.isEmpty() && nums[stack.peek()] < curr) {
+                res[stack.pop()] = curr;
+            }
+
+            if (i < n) {
+                stack.push(i);
+            }
+        }
+
+        return res;
+    }
+}
