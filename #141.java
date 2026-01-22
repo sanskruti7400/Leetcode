65e8944e99ddf124eff5b8b470e7fb71b2dd5b52
Sanskruti Dhanaji Kadam <sanskadam7400@gmail.com>
2026-01-22 23:10:11 +0530

    Create #141.java

---
+++ #141.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null){
+            return false;
+        }
+        ListNode trav1=head;
+        ListNode trav2=head;
+        while(trav2!=null && trav2.next!=null){
+            trav1=trav1.next;
+            trav2=trav2.next.next;
+
+            if(trav1==trav2){
+                return true;
+            }
+        }
+        return false;
+    }
+}
