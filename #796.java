448d0de8d3faca88588f759e21d9897e74fc6bf6
Sanskruti Dhanaji Kadam <sanskadam7400@gmail.com>
2026-02-05 21:41:15 +0530

    Create #796.java

---
+++ #796.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+
+        if(s.length()!=goal.length()){
+            return false;
+        }
+        s=s.concat(s);
+
+        if(s.contains(goal)){
+            return true;
+        }
+        return false;
+    }
+}
